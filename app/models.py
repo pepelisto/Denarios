08c93512ebecbd332a5504cf24d6b172a8426461
pepelisto <martinmoran422@gmail.com>
2023-10-08 13:52:17 -0300

    Initial commit

--- app/models.py
+++ app/models.py
@@ -39,6 +39,8 @@ class Open_position(models.Model):
     stopPrice_precision = models.IntegerField(null=True, default=0)
     timeframe = models.IntegerField(null=True, default=0)
     sl_order_id = models.CharField(max_length=50, choices=types, null=True, default=None)
+    def __str__(self):
+        return str(self.symbol) + ' - ' + self.type + ' - ' + str(self.alt_TP_SL) + ' - ' + str(self.timeframe)
 
 class Closed_position(models.Model):
     symbol = models.ForeignKey(Symbol, on_delete=models.CASCADE)
