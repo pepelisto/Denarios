babf1783748ec2a1e0b6d6cb792f4afb0ed6a331
pepelisto <martinmoran422@gmail.com>
2023-11-04 15:47:55 -0300

    Merge remote-tracking branch 'origin/master'

---
+++ bots/A_A_9/snipets/setQuantity.py
@@ -0,0 +1,12 @@
+from django.conf import settings
+import django
+from bots.A_A_9.functions.Take_position import BinanceTrader
+
+from Denarios.settings import DATABASES, INSTALLED_APPS
+settings.configure(DATABASES=DATABASES, INSTALLED_APPS=INSTALLED_APPS)
+django.setup()
+
+from app.models import *
+sy = Optimum_parameter.objects.get(symbol__symbol='BTCUSDT')
+sy.q = 120
+sy.save()
